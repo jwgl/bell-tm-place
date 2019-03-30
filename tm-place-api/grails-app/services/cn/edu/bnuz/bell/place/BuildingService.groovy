d26d1b727d4a715cd582e6e9f324b0dcc8b8f441
Yang Lin <todoubaba@gmail.com>
2019-03-30 23:17:24 +0800

    Add building keeper support (#89)

--- tm-place-api/grails-app/services/cn/edu/bnuz/bell/place/BuildingService.groovy
+++ tm-place-api/grails-app/services/cn/edu/bnuz/bell/place/BuildingService.groovy
@@ -6,15 +6,15 @@ import grails.gorm.transactions.Transactional
 
 @Transactional(readOnly = true)
 class BuildingService {
-    List<String> findBuildings(Term term, UserType userType) {
+    List<String> findBuildings(Term term, UserType userType, Boolean isAdmin) {
         Place.executeQuery '''
 select distinct place.building
 from PlaceUserType placeUserType
 join placeUserType.place place
 left join place.allowBookingTerms placeBookingTerm
-where placeUserType.userType = :userType
+where (placeUserType.userType = :userType or :isAdmin = true)
 and (place.enabled = true or placeBookingTerm.term = :term)
 order by place.building
-''', [term: term, userType: userType]
+''', [term: term, userType: userType, isAdmin: isAdmin]
     }
 }
